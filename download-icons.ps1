6f192c4dd6c217906313fd613f3ee5a1a504acbe
Claudio De Valera Washington ATSOU <atsouclaudio@gmail.com>
2026-05-02 01:21:32 +0000

    Add files via upload

---
+++ download-icons.ps1
@@ -0,0 +1,7 @@
+$icons = @('python','javascript','java','html5','css3','c','csharp','mysql','postgresql','mariadb','git','github','vscode','docker','redis','nginx','tailwindcss','reactnative','flutter','django','fastapi','dotnetcore')
+foreach ($ic in $icons) {
+    $url = "https://cdn.jsdelivr.net/gh/devicons/devicon@v2.16.0/icons/$ic/$ic-original.svg"
+    $out = "c:\Users\hp\Documents\PortofolioA\icons\$ic-original.svg"
+    Invoke-WebRequest -Uri $url -OutFile $out
+}
+Write-Host "Done!"
